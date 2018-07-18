ef1a5f74acd1b7d859c1404b7ecae20e4d1c5170
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 15:30:44 +0100

    Initial add

---
+++ bank_account_functions.rb
@@ -0,0 +1,54 @@
+ACCOUNTS = [
+  {
+    holder_name: "Jay",
+    amount: 55,
+    type: "business"
+  },
+  {
+    holder_name: "Rick",
+    amount: 1,
+    type: "personal"
+  },
+  {
+    holder_name: "Keith",
+    amount: 500,
+    type: "business"
+  },
+  {
+    holder_name: "Valerie",
+    amount: 100,
+    type: "personal"
+  },
+  {
+    holder_name: "Michael",
+    amount: 5,
+    type: "personal"
+  },
+  {
+    holder_name: "Kate",
+    amount: 2000,
+    type: "business"
+  },
+  {
+    holder_name: "Tony",
+    amount: 150,
+    type: "personal"
+  },
+  {
+    holder_name: "Sandy",
+    amount: 4500,
+    type: "business"
+  }
+]
+
+def number_of_bank_accounts()
+
+end
+
+def first_bank_account_holder()
+
+end
+
+def bank_account_owner_names()
+
+end
